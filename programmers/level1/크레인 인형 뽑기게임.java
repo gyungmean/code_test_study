00501c054622342476aff63c78ae92856db4518a
gyungmean <70059000+gyungmean@users.noreply.github.com>
2022-02-16 22:23:28 +0900

    프로그래머스 level1 크레인 인형뽑기 게임.java

---
+++ programmers/level1/크레인 인형 뽑기게임.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> bucket = new Stack<Integer>();
+        
+        for(int m : moves){
+            int i = 0;
+            while(i < board.length - 1){
+                if(board[i][m] != 0) break;
+                if(board[i][m] == 0){
+                i++;
+                }
+            }
+            if(i >= board.length) continue;
+            if(bucket.empty() != true){
+                if(bucket.peek() != board[i][m]){
+                    bucket.push(board[i][m]);
+                }
+                else{
+                    answer++;
+                    bucket.pop();
+                }
+        }
+            else{
+                bucket.push(board[i][m]);
+            }
+        
+    }
+        return answer;
+}
+}
