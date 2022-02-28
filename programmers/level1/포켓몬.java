144cccebfada39f4b74e555cefa61ae0a78e08b1
gyungmean <70059000+gyungmean@users.noreply.github.com>
2022-03-01 00:00:19 +0900

    프로그래머스 level1 포켓몬

--- programmers/level1/포켓몬.java
+++ programmers/level1/포켓몬.java
@@ -2,14 +2,7 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int count = nums.length / 2;
-        Arrays.sort(nums);
         int[] new_nums = Arrays.stream(nums).distinct().toArray();
-        int answer = count;
-        if(new_nums.length >= count){
-            return answer;
-        }
-        else{
-            return new_nums.length;
-        }
+        return new_nums.length >= count ? count : new_nums.length;
     }
 }
