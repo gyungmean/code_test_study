187a7dc0f228efe859a60c7b99284f93f3fc6210
gyungmean <70059000+gyungmean@users.noreply.github.com>
2022-03-04 00:17:22 +0900

    프로그래머스 level1 실패율

--- programmers/level1/실패율.java
+++ programmers/level1/실패율.java
@@ -1,18 +1,24 @@
 import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
-        Map<Integer, Double> fail = new HashMap<>();
+        Map<Integer, Double> rate = new HashMap<>();
         for(int i = 1; i <= N; i++){
-            int stage_count = 0;
-            int user_count = 0;
+            int stage_count = 0; //클리어하지 못 한 사람 수
+            int user_count = 0; //도전한 사람 수 
             for(int s : stages){
                 if(s >= i) { user_count++; }
                 if(s == i) { stage_count++;}
             }
-            fail.put(i, (double)stage_count / user_count);
+            if(user_count == 0){
+                rate.put(i, 0.0);
+            }
+            else{
+                rate.put(i, (double)stage_count / user_count);
+            }
+            
         }
         
-        List<Map.Entry<Integer, Double>> fail_list = new LinkedList<>(fail.entrySet());
+        List<Map.Entry<Integer, Double>> fail_list = new LinkedList<>(rate.entrySet());
         fail_list.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
         
         int[] answer = new int[N];
