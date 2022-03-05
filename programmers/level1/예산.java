605492c38b6d821ab12e1c4478e408ea6b9f0414
gyungmean <70059000+gyungmean@users.noreply.github.com>
2022-03-06 00:06:54 +0900

    프로그래머스 level1 예산

--- programmers/level1/예산.java
+++ programmers/level1/예산.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
+        Arrays.sort(d);
+        for(int amount : d){
+            budget -= amount;
+            if(budget < 0) { break; }
+            answer++;
+        }
         return answer;
     }
 }
