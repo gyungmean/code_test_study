1239902551d24f6c2878f2eb2b9e72afc9581fa1
gyungmean <70059000+gyungmean@users.noreply.github.com>
2022-03-18 21:48:14 +0900

    프로그래머스 level1

---
+++ programmers/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = new String[strings.size()];
+        Arrays.sort(strings);
+        String[] = new String[strings.size()];
+        
+        for(int i = 0; i < strings.size(); i++){
+            s.charAt(i).subString;
+        }
+        return answer;
+    }
+}
