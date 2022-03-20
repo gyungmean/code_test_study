537d1501ae53f014fc80d1ff17d4a2e335aaee8b
gyungmean <70059000+gyungmean@users.noreply.github.com>
2022-03-20 23:19:48 +0900

    프로그래머스 level1 문자열 내 마음대로 정렬하기

--- programmers/level1/문자열 내 마음대로 정렬하기.java
+++ programmers/level1/문자열 내 마음대로 정렬하기.java
@@ -1,13 +1,26 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] strings, int n) {
-        String[] answer = new String[strings.size()];
         Arrays.sort(strings);
-        String[] = new String[strings.size()];
-        
-        for(int i = 0; i < strings.size(); i++){
-            s.charAt(i).subString;
+        List<Character> arr_list = new ArrayList<>();
+        for(String s : strings){
+            arr_list.add(s.charAt(n));
         }
+        
+        Set<Character> set = new HashSet<Character>(arr_list);
+        arr_list =new ArrayList<Character>(set);
+
+        Collections.sort(arr_list);
+        List<String> answer_list = new ArrayList<>();
+        for(char c : arr_list){
+            for(String s : strings){
+                if(s.charAt(n) == c){
+                    answer_list.add(s);
+                }
+            }
+        }
+                    
+        String[] answer = answer_list.toArray(String[]::new);
         return answer;
     }
 }
