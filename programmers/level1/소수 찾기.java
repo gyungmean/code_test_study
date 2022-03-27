eddb685d3a89c3de2a980cb6eb5db8e0ec52bc56
gyungmean <70059000+gyungmean@users.noreply.github.com>
2022-03-27 22:46:10 +0900

    Update 소수 찾기.java

--- programmers/level1/소수 찾기.java
+++ programmers/level1/소수 찾기.java
@@ -9,7 +9,7 @@ class Solution {
         return answer;
     }
     public boolean isPrime(int n){
-        for(int i = 2; i < n; i++){
+        for(int i = 2; i*i < n; i++){
             if(n % i == 0){
                 return false;
             }
