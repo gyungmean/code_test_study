5da2156bf8a034394fe289f8125e5cd1b8a3ceef
gyungmean <70059000+gyungmean@users.noreply.github.com>
2022-03-31 15:55:22 +0900

    프로그래머스 level1 소수 찾기

--- programmers/level1/소수 찾기.java
+++ programmers/level1/소수 찾기.java
@@ -1,19 +1,20 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i = 2; i <= n; i++){//지우기
-            if(isPrime(i)){
-                answer++;
+        boolean[] prime_checks = new boolean[n + 1]; //true : 소수 아님 false : 소수
+        prime_checks[0] = true;
+        prime_checks[1] = true;
+
+        for(int i = 2; i * i <= n; i++){
+            if(!prime_checks[i]){
+                for(int j = i * i; j <= n; j += i){ 
+                    prime_checks[j] = true;
+                }
             }
+        }
+        for(boolean prime_check : prime_checks){
+            if(!prime_check) {answer++;}
         }
         return answer;
-    }
-    public boolean isPrime(int n){
-        for(int i = 2; i*i < n; i++){
-            if(n % i == 0){
-                return false;
-            }
-        }
-        return true;
     }
 }
