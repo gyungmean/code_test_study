05cfd3652453cba010e55c3c6669f6eb02672a39
gyungmean <70059000+gyungmean@users.noreply.github.com>
2022-05-02 23:56:10 +0900

    프로그래머스 level2 오픈채팅방

--- programmers/level2/오픈채팅방.java
+++ programmers/level2/오픈채팅방.java
@@ -1,1 +1,33 @@
-//
+import java.util.*;
+class Solution {
+    public String[] solution(String[] record) {
+        List<String> answer_list = new ArrayList<>();
+        HashMap<String, String> user = new HashMap<>();
+        
+        for(String r : record){
+            String[] tmp = r.split(" ");
+            switch(tmp[0]){
+                    case "Enter":
+                    user.put(tmp[1], tmp[2]);
+                    break;
+                    case "Change":
+                    user.put(tmp[1], tmp[2]);
+                    break;
+            }
+        }
+        for(String r : record){
+            String[] tmp = r.split(" ");
+
+            switch(tmp[0]){
+                    case "Enter":
+                    answer_list.add(user.get(tmp[1]) + "님이 들어왔습니다.");
+                    break;
+                    case "Leave":
+                    answer_list.add(user.get(tmp[1]) + "님이 나갔습니다.");
+                    break;
+        }
+    }
+        String[] answer = answer_list.toArray(String[] :: new);
+        return answer;
+    }
+}
