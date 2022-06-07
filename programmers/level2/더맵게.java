5f71eb57de00b83f690b5e44287c27612ba02a9b
gyungmean <70059000+gyungmean@users.noreply.github.com>
2022-06-07 23:50:44 +0900

    Update 더맵게.java

--- programmers/level2/더맵게.java
+++ programmers/level2/더맵게.java
@@ -1,1 +1,16 @@
-///
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        Arrays.sort(scoville);
+        int answer = 0;
+        int new_food = 0;
+        for(int i = 0; i < scoville.length - 1; i++){
+            if(scoville[i] >= K) break;
+            new_food = Math.min(scoville[i], scoville[i + 1]) + Math.max(scoville[i], scoville[i + 1]) * 2;
+            scoville[i + 1] = new_food;
+            answer++;
+        }
+        if(scoville[scoville.length - 1] >= K) return answer;
+        else return -1;
+    }
+}
